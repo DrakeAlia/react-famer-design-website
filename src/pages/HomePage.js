18a6d6aa01754701cff25119c3da56a035351c3a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-28 16:28:34 -0700

    finished the content functionality and styles, and imported content in homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Clients from "../components/Clients/Clients";
+import Content from "../components/Content/Content";
 import Features from "../components/Features/Features";
 import Hero from "../components/Hero/Hero";
+import { heroOne, heroTwo, heroThree, heroFour } from "../data/HeroData"
+import { Heading } from "../globalStyles";
 
 const HomePage = () => {
   return (
@@ -9,6 +12,11 @@ const HomePage = () => {
       <Hero />
       <Features />
       <Clients />
+      <Heading id='projects'>Our Projects</Heading>
+      <Content {...heroOne} />
+      <Content {...heroTwo} />
+      <Content {...heroThree} />
+      <Content {...heroFour} />
     </>
   );
 };
