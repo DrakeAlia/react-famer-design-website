c156836c68a4a10ca892fdb0fad9e0435a4f9d5e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-27 21:48:42 -0700

    setup the content and content styles

---
+++ src/components/Content/Content.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { Container } from '../../globalStyles';
+import {
+    ContentSec,
+    ContentRow,
+    TextWrapper,
+    TopLine,
+    Heading,
+    ContentButton,
+    Subtitle,
+    ImgWrapper,
+    Img,
+    ContentColumn,
+} from './ContentStyles';
+import { useInView } from 'react-intersection-observer';
+import { useAnimation } from 'framer-motion';
+import Tilt from 'react-tilt';
+
+const Content = () => {
+  return <div>Content</div>;
+};
+
+export default Content;
